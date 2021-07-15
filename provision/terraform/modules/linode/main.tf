1e65cc34c8fcf569dc2993568e9e20e6e3504e84
Jigar Wala <jigarwala007@gmail.com>
2021-07-16 01:17:00 +0530

    change instance type, add firewall

--- provision/terraform/modules/linode/main.tf
+++ provision/terraform/modules/linode/main.tf
@@ -2,11 +2,31 @@ resource "linode_instance" "node" {
   label           = var.node_label
   image           = "linode/ubuntu20.04"
   region          = "ap-west"
-  type            = "g6-standard-2"
+  type            = "g6-nanode-1"
   authorized_keys = [linode_sshkey.itsjwala.ssh_key]
 }
 
 resource "linode_sshkey" "itsjwala" {
   label   = "terraform"
   ssh_key = chomp(file("~/.ssh/id_rsa.pub"))
+}
+
+
+resource "linode_firewall" "my_firewall" {
+  label = "my_firewall"
+
+  inbound {
+    label    = "allow-ssh"
+    action   = "ACCEPT"
+    protocol = "TCP"
+    ports    = "22"
+    ipv4     = ["0.0.0.0/0"]
+  }
+
+  inbound_policy = "DROP"
+
+  outbound_policy = "ACCEPT"
+
+  linodes = [linode_instance.node.id]
+  depends_on = [ linode_instance.node ]
 }
